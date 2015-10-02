1cdf2250b02253822e7fe443f12989b37456025d
Omer Gil <Omer.gil89@gmail.com>
2015-10-02 18:14:57 +0300

    omer

--- MVC/src/boot/Run.java
+++ MVC/src/boot/Run.java
@@ -9,7 +9,7 @@ import presenter.Presenter;
 import view.CLI;
 import view.CLIObservableView;
 
-public class Run {//test!!2!!!
+public class Run {//TEST
 
 	public static void main(String[] args) throws InterruptedException {
 		CLIObservableView ui = new CLIObservableView(new CLI(System.in, System.out));
